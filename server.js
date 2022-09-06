60996e53408946478e8e7610f17231cf29e14def
Mersant <dlowry99@outlook.com>
2022-09-06 15:11:22 -0400

    Update server.js
    Post request for /api/notes now functions

--- server.js
+++ server.js
@@ -16,12 +16,50 @@ app.use(express.static('public'));
 
 // API GET Routes
 app.get('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/db/db.json'))
+  //res.sendFile(path.join(__dirname, '/db/db.json'))
+  fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    res.json(JSON.parse(data))
+  })
 );
 // API POST Routes
-app.post('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/db/db.json'))
-);
+app.post('/api/notes', (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+    };
+
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          const parsedData = JSON.parse(data);
+          parsedData.push(newNote);
+          fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(parsedData, null, 4), (err) => {
+            if (err) {
+                console.error(err);
+            }
+          }
+          );
+        }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in adding note');
+  }
+});
 
 // HTML GET Routes
 app.get('/notes', (req, res) =>
