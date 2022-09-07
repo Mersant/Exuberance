cc1b863d587c0ec58fa474c4b40ae123c5e90d48
Mersant <dlowry99@outlook.com>
2022-09-06 23:02:23 -0400

    Update server.js
    Cleaned up code and added more comments

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-//const api = require('./public/assets/js/index.js');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,13 +9,11 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use('/api', api);
 
 app.use(express.static('public'));
 
 // API GET Routes
 app.get('/api/notes', (req, res) =>
-  //res.sendFile(path.join(__dirname, '/db/db.json'))
   fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', (err, data) => {
     if (err) {
       console.error(err);
@@ -25,9 +22,11 @@ app.get('/api/notes', (req, res) =>
     res.json(JSON.parse(data))
   })
 );
+
 // API POST Routes
 app.post('/api/notes', (req, res) => {
   const { title, text } = req.body;
+  // newNote will be appended to the db.json file as well as returned in the response
   var newNote;
 
   if (title && text) {
@@ -71,8 +70,12 @@ app.delete('/api/notes/:id', (req, res) => {
         if (err) {
           console.error(err);
         } else {
+          // Current db.json file
           const parsedData = JSON.parse(data);
+          // Buffer for the new db.json file
           var newData = [];
+
+          // Push every note in parsedData except the deleted one to newData[]
           for(var i=0; i<Object.keys(parsedData).length; i++) {
             if(parsedData[i]["id"] != req.params.id) {
                 parsedData[i]["id"] = newData.length;
@@ -80,6 +83,7 @@ app.delete('/api/notes/:id', (req, res) => {
             }
           }
           
+          // Write newData[] to db.json
           fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(newData, null, 4), (err) => {
             if (err) {
                 console.error(err);
